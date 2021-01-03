1c954bbb07090b1014ea7ad64db730419e3be485
joeb3219 <joseph.a.boyle@rutgers.edu>
2021-01-03 04:31:14 -0800

    Fixes paging problems in calendar

--- client/src/app/services/meeting.service.ts
+++ client/src/app/services/meeting.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Course, Meeting, MeetingType, StandardResponseInterface } from '@dynrec/common';
 import { environment } from '@environment';
+import { getFilterParams } from 'app/decorators';
 
 @Injectable()
 export class MeetingService {
@@ -9,19 +10,24 @@ export class MeetingService {
 
     public async getMeetingTime(course: Course): Promise<StandardResponseInterface<Meeting<MeetingType>[]>> {
         const url = `${environment.apiURL}/course/${course.id}/meetings`;
+
+        const params = getFilterParams({ limit: -1 });
+
         return new Promise((resolve, reject) => {
-            this.http.get<StandardResponseInterface<Meeting<MeetingType>[]>>(url).subscribe(
-                result => {
-                    if (result) {
-                        // eslint-disable-next-line no-param-reassign
-                        result.data = result.data.map(item => new Meeting(item));
-                        resolve(result);
-                    } else reject(new Error('No result returned'));
-                },
-                (err: Error) => {
-                    reject(err);
-                }
-            );
+            this.http
+                .get<StandardResponseInterface<Meeting<MeetingType>[]>>(url, { params })
+                .subscribe(
+                    result => {
+                        if (result) {
+                            // eslint-disable-next-line no-param-reassign
+                            result.data = result.data.map(item => new Meeting(item));
+                            resolve(result);
+                        } else reject(new Error('No result returned'));
+                    },
+                    (err: Error) => {
+                        reject(err);
+                    }
+                );
         });
     }
 }
